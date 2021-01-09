013c93d6e659536c828db156e216c3724ae5c93c
Michał Sieczkowski <michal@ethworks.io>
2021-01-09 17:40:41 +0100

    Extract convert_to_tf_types function

--- train.py
+++ train.py
@@ -14,19 +14,7 @@ def train():
     train_df = df[0:train_cutoff]
     test_df = df[train_cutoff + 1: total_samples]
 
-    train_dataset = tf.data.Dataset.from_tensor_slices(
-        (tf.cast(train_df['text'].values, tf.string), tf.cast(train_df['label'].values, tf.int32)))
-    test_dataset = tf.data.Dataset.from_tensor_slices(
-        (tf.cast(test_df['text'].values, tf.string), tf.cast(test_df['label'].values, tf.int32)))
-
-    # creates len(dataset)/BATCH_SIZE batches. Shuffle takes first BUFFER_SIZE elements from dataset,
-    # than randomly takes BATCH_SIZE elements from buffer. Those elements are replaced with next ones from original
-    # dataset. This is repeated until are samples are consumed.
-    # prefetching allows later elements to be prepared while the current element is being processed. Improves latency
-    # and throughput
-    train_dataset = train_dataset.shuffle(config.BUFFER_SIZE).batch(config.BATCH_SIZE).prefetch(
-        tf.data.experimental.AUTOTUNE)
-    test_dataset = test_dataset.batch(config.BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+    train_dataset, test_dataset = convert_to_tf_types(train_df, test_df)
 
     if config.BERT["ENABLED"]:
         print('Training BERT model')
@@ -54,6 +42,25 @@ def train():
         print('Evaluation of RNN model on test data')
         evaluate_model(y_pred_proba, test_df['label'], classes)
         tf.keras.backend.clear_session()
+
+
+def convert_to_tf_types(train_df, test_df):
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+        (tf.cast(train_df['text'].values, tf.string), tf.cast(train_df['label'].values, tf.int32)))
+    test_dataset = tf.data.Dataset.from_tensor_slices(
+        (tf.cast(test_df['text'].values, tf.string), tf.cast(test_df['label'].values, tf.int32)))
+
+    # creates len(dataset)/BATCH_SIZE batches. Shuffle takes first BUFFER_SIZE elements from dataset,
+    # than randomly takes BATCH_SIZE elements from buffer. Those elements are replaced with next ones from original
+    # dataset. This is repeated until are samples are consumed.
+    # prefetching allows later elements to be prepared while the current element is being processed. Improves latency
+    # and throughput
+
+    train_dataset = train_dataset.shuffle(config.BUFFER_SIZE).batch(config.BATCH_SIZE).prefetch(
+        tf.data.experimental.AUTOTUNE)
+    test_dataset = test_dataset.batch(config.BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+
+    return train_dataset, test_dataset
 
 
 if __name__ == '__main__':
